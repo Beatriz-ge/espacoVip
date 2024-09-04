7b8c85296848cd4be98f579637775dd34d8fe825
Beatriz-ge <bibi.geane2@gmail.com>
2024-09-04 11:27:28 -0300

    feat: carousel extern area function

--- espacovip-web/src/components/carousel-out.jsx
+++ espacovip-web/src/components/carousel-out.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../styles/carousel.css';
-import Piscina6 from '../assets/piscina/piscina6.jpeg';
+import Churrasqueira1 from '../assets/area-externa/churrasqueira1.jpeg';
+import Grama1 from '../assets/area-externa/grama1.jpeg';
+import Grama2 from '../assets/area-externa/grama2.jpeg';
+import Grama3 from '../assets/area-externa/grama3.jpeg';
+import Grama4 from '../assets/area-externa/grama4.jpeg';
+import Parquinho1 from '../assets/area-externa/parquinho1.jpeg';
+import Parquinho2 from '../assets/area-externa/parquinho2.jpeg';
+import Piscina3 from '../assets/piscina/piscina3.jpeg';
+import Piscina5 from '../assets/piscina/piscina5.jpeg';
 
 function Carousel() {
   return (
@@ -30,24 +38,108 @@ function Carousel() {
               data-bs-slide-to="2"
               aria-label="Slide 3"
             ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="3"
+              aria-label="Slide 4"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="4"
+              aria-label="Slide 5"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="5"
+              aria-label="Slide 6"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="6"
+              aria-label="Slide 7"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="7"
+              aria-label="Slide 8"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="8"
+              aria-label="Slide 9"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="9"
+              aria-label="Slide 10"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark2"
+              data-bs-slide-to="10"
+              aria-label="Slide 11"
+            ></button>
           </div>
           <div className="carousel-inner">
             <div className="carousel-item active" data-bs-interval="10000">
-              <img src={Piscina6} className="d-block w-100" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
+              <img src={Grama4} className="d-block w-100" alt="Grama 5" />
+              <div className="carousel-caption">
+                <h5>Casa e piscina</h5>
               </div>
             </div>
             <div className="carousel-item" data-bs-interval="2000">
-              <img  className="d-block w-100" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
+              <img src={Churrasqueira1} className="d-block w-100" alt="Churrasqueira 1" />
+              <div className="carousel-caption">
+                <h5>Churrasqueira</h5>
               </div>
             </div>
             <div className="carousel-item">
-              <img  className="d-block w-100" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
+              <img src={Grama3} className="d-block w-100" alt="Grama 3" />
+              <div className="carousel-caption">
+                <h5>Gramado</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Grama1} className="d-block w-100" alt="Grama 1" />
+              <div className="carousel-caption">
+                <h5>Piscina e casa</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Grama2} className="d-block w-100" alt="Grama 8" />
+              <div className="carousel-caption">
+                <h5>Casa</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Parquinho1} className="d-block w-100" alt="Parquinho 1" />
+              <div className="carousel-caption">
+                <h5>Parquinho</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Parquinho2} className="d-block w-100" alt="Parquinho 2" />
+              <div className="carousel-caption">
+                <h5>Parquinho</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Piscina3} className="d-block w-100" alt="Piscina 3" />
+              <div className="carousel-caption">
+                <h5>Piscina</h5>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={Piscina5} className="d-block w-100" alt="Piscina 5" />
+              <div className="carousel-caption">
+                <h5>Piscina</h5>
               </div>
             </div>
           </div>
